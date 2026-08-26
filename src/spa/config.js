7a532cec570e2db9bbd92f79af587d7295116f3b
Jarred Barber <jpb5082@gmail.com>
2026-08-25 20:54:06 -0400

    drive oauth+cleanup

--- src/spa/config.js
+++ src/spa/config.js
@@ -1,6 +1,6 @@
 // Public OAuth configuration. Client IDs and Worker origins are public; secrets never appear here.
 window.GLINT_CONFIG = {
-    driveClientId: '962284089942-cs2p2j1i268bt9q6kkcqd64omvpvfmaj.apps.googleusercontent.com',
+    driveClientId: '833335009551-iar5dhp95n4o753f41snc05ls89i28er.apps.googleusercontent.com',
     githubClientId: 'Ov23liHgd7cWeAo4KjRZ',
     githubOAuthWorkerOrigin: 'https://glint-github-oauth.hector-ea.workers.dev',
     githubRedirectUri: 'https://jarredbarber.github.io/glint/',
